5c3ef25ed75d7fb465f3b61adaf28c184fda0693
Carson Wilde <wilde.utah@gmail.com>
2020-12-11 12:42:11 -0700

    created basic car model

--- dealership/models.py
+++ dealership/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    price = models.FloatField()
+
+    def __str__(self):
+        return str(self.make) + " " + str(self.model)
