b984da67036d346379a8599c0d56d85cdee568fc
Carson Wilde <wilde.utah@gmail.com>
2020-12-11 13:03:49 -0700

    root url now displays some information about a car

--- dealership/views.py
+++ dealership/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    if len(Car.objects.all()) == 0:
+        make_dummy_car()
+    res = Car.objects.all()[0]
+    return HttpResponse("car: {}".format(res))
+
+def make_dummy_car():
+    c = Car(make="Ford", model="F150", color="grey", price=50000.00)
+    c.save()
