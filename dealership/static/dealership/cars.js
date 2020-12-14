5af1efe312b031bf9e54cc6208e1f274721d7267
Carson Wilde <carson.wilde@aggiemail.usu.edu>
2020-12-13 17:57:50 -0700

    began styling the web page, few bugs

---
+++ dealership/static/dealership/cars.js
@@ -0,0 +1,19 @@
+//create top of document div
+var div1 = document.createElement('div');
+div1.setAttribute('class', 'blue top_container');
+document.querySelector('body').appendChild(div1);
+
+var logo = document.createElement('img');
+logo.setAttribute('src', 'dealership_logo.jpg');
+logo.setAttribute('alt', 'dealership logo')
+logo.setAttribute('class', 'logo_image')
+div1.appendChild(logo)
+
+var title = document.createElement('h1');
+title.textContent = "SBDC Dealership"
+div1.appendChild(title)
+
+//create right of document div
+var div2 = document.createElement('div');
+div2.setAttribute('class', 'blue left_container');
+document.querySelector('body').appendChild(div2);
